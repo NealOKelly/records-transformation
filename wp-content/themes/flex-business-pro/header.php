d8c4400ba835ca51547a36c2e97d70dab994b68f
Neal O'Kelly <neal.okelly100@gmail.com>
2020-03-15 15:22:15 +0000

    Add Records Transformation favicon.

--- wp-content/themes/flex-business-pro/header.php
+++ wp-content/themes/flex-business-pro/header.php
@@ -35,6 +35,27 @@ do_action( 'flex_business_pro_action_head' );
 ?>
 
 <?php wp_head(); ?>
+
+<!-- START - Add favicons -->
+<link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo('template_directory'); ?>/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo('template_directory'); ?>/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('template_directory'); ?>/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php bloginfo('template_directory'); ?>/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('template_directory'); ?>/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php bloginfo('template_directory'); ?>/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('template_directory'); ?>/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo('template_directory'); ?>/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_directory'); ?>/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="<?php bloginfo('template_directory'); ?>/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_directory'); ?>/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="<?php bloginfo('template_directory'); ?>/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('template_directory'); ?>/favicon-16x16.png">
+<link rel="manifest" href="<?php bloginfo('template_directory'); ?>/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="<?php bloginfo('template_directory'); ?>/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">	
+<!-- END - Add favicons -->
+	
 </head>
 
 <body <?php body_class(); ?>>
