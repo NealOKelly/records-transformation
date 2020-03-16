b4cdc9d48d3d15dd7d8d981235d43ab6852e1f85
Neal O'Kelly <neal.okelly100@gmail.com>
2020-03-16 15:07:16 +0000

    Remove hard-coded video section from front page.

--- wp-content/themes/flex-business-pro/front-page.php
+++ wp-content/themes/flex-business-pro/front-page.php
@@ -64,36 +64,6 @@ if ( 'posts' != get_option( 'show_on_front' ) ){
                     </section>
                 <?php endif; ?>
 
-				<section id="watch" class="page-section">
-					<div class="wrapper">
-						<div class="section-header">
-							<h2 class="section-title">Watch</h2>
-						</div><!-- .section-header -->
-
-						<div class="section-content">
-							<article>
-								<div class="entry-content">
-									<div class="wp-block-columns">
-										<div class="wp-block-column" style="flex-basis:25%"></div>
-										<div class="wp-block-column" style="flex-basis:50%">
-											<figure class="wp-block-embed-youtube wp-block-embed is-type-video is-provider-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio">
-												<div class="wp-block-embed__wrapper">
-													<iframe title="How An Information Asset Register Can Help Keep Your Business Safe" src="https://www.youtube.com/embed/KnDB3T3R2u0?feature=oembed" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" width="640" height="360" frameborder="0"></iframe>
-												</div>
-											</figure>
-											<div class="social-sharing ss-social-sharing">
-												<a onclick="return ss_plugin_loadpopup_js(this);" rel="external nofollow" class="ss-button-twitter" href="http://twitter.com/intent/tweet/?text=Case+Study&url=http%3A%2F%2Fwww.recordstransformation.co.uk%2Fcase-study%2F&via=arjun077" target="_blank">Share on Twitter</a>
-												<a onclick="return ss_plugin_loadpopup_js(this);" rel="external nofollow" class="ss-button-linkedin" href="http://www.linkedin.com/shareArticle?mini=true&url=http%3A%2F%2Fwww.recordstransformation.co.uk%2Fcase-study%2F&title=Case+Study" target="_blank" >Share on Linkedin</a>	
-											</div>
-										</div>
-										<div class="wp-block-column" style="flex-basis:25%"></div>
-									</div>
-								</div>
-							</article>
-						</div>
-					</div>
-			</section>
-
             <?php } elseif( $section['id'] == 'counter' ) { ?>
                 <?php $disable_counter_section = flex_business_pro_get_option( 'disable_counter_section' );
                 $background_counter_section = flex_business_pro_get_option( 'background_counter_section' );
